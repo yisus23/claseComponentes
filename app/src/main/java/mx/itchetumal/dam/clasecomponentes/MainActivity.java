5d96ad65ef3a6fb7ae720643035dc1bc873da3ae
jesus <choy_n4@hotmail.com>
2018-09-14 19:06:35 -0500

    interface principal terminada. inicio de programacion

--- app/src/main/java/mx/itchetumal/dam/clasecomponentes/MainActivity.java
+++ app/src/main/java/mx/itchetumal/dam/clasecomponentes/MainActivity.java
@@ -2,6 +2,8 @@ package mx.itchetumal.dam.clasecomponentes;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,4 +12,39 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
+
+    public void GenerarRFC(View v) {
+        //tomar la primera letra y primer vocal del apellido paterno
+        EditText edtApellidoPaterno = (EditText) findViewById(R.id.edtApellidoPaterno);
+        char primeraLetraP = edtApellidoPaterno.getText().charAt(0);
+
+
+       char [] vocales = {'A' ,'E' ,'I' ,'O' ,'U' ,'a' ,'e' ,'i' ,'o' ,'u', 'á' ,'é' ,'í' ,'ó' ,'ú'};
+        //tomar la ptimera letra el apelido materno
+
+        //tomar la primera letra del nombre
+
+        //tomar los ultimos digitos del año de nacimiento
+
+
+        //tomar los dos digitos del mes de nacimiento
+
+
+        // tomar los dos digitos del dia del nacimiento
+
+        // generar  3 caracteres (Letras mayusculas o numeros )para la homonimia
+
+
+
+        //concatenar lo anterior y mostrar el RFC
+    }
+
+
+
+
+
+
+
+
 }
