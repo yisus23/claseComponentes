699eb49a4be1f123195c4b7b81e60bf452f6a4bb
yisus23 <choyn4@gmail.com>
2018-09-21 18:58:00 -0500

    aplicacion de RFC que obtiene las dos primeras letras del apellido paterno y una del materno

--- app/src/main/java/mx/itchetumal/dam/clasecomponentes/MainActivity.java
+++ app/src/main/java/mx/itchetumal/dam/clasecomponentes/MainActivity.java
@@ -4,6 +4,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.EditText;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -12,19 +13,49 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+    public String adecuarCadena(String cadena){
+        String cadAux = cadena.toUpperCase().trim();
+        char [] vocalesAcentuadas = {'Á' ,'É' ,'Í' ,'Ó' ,'Ú' };
+        char [] vocales = {'A' ,'E' ,'I' ,'O' ,'U' };
+        for (byte pos  = 0; pos < vocalesAcentuadas.length; pos++){
+        cadAux.replace(vocalesAcentuadas[pos],vocales[pos]);
 
+        }
+        return cadAux;
+    }
 
-    public void GenerarRFC(View v) {
+    public void generarRFC(View v) {
         //tomar la primera letra y primer vocal del apellido paterno
         EditText edtApellidoPaterno = (EditText) findViewById(R.id.edtApellidoPaterno);
-        char primeraLetraP = edtApellidoPaterno.getText().charAt(0);
+        String apPaterno  = adecuarCadena(edtApellidoPaterno.getText().toString());
 
 
-       char [] vocales = {'A' ,'E' ,'I' ,'O' ,'U' ,'a' ,'e' ,'i' ,'o' ,'u', 'á' ,'é' ,'í' ,'ó' ,'ú'};
-        //tomar la ptimera letra el apelido materno
+       Toast.makeText(this, "Cadena convertida" + apPaterno, Toast.LENGTH_LONG).show();
 
+
+        char primeraLetraP = apPaterno.charAt(0);
+        char[] vocales = {'A' ,'E' ,'I' ,'O' ,'U' };
+        char primeravocal = ' ';
+        byte pos;
+        boolean encontrada = false;
+        for (pos  = 0; pos < vocales.length; pos++){
+            if (apPaterno.indexOf(vocales[pos])>=0){
+                encontrada = true;
+                break;
+            }
+        }
+        if (encontrada){
+            primeravocal = vocales[pos];
+        }
+
+
+
+         //tomar la ptimera letra el apelido materno
+        EditText edtApellidoMaterno = (EditText) findViewById(R.id.edtApellidoMaterno);
+        char primeraLetraM  = edtApellidoMaterno.getText().charAt(0);
         //tomar la primera letra del nombre
-
+        EditText edtNombre = (EditText) findViewById(R.id.edtNombre);
+        char primeraLetraNombre = edtNombre.getText().charAt(0);
         //tomar los ultimos digitos del año de nacimiento
 
 
@@ -33,7 +64,7 @@ public class MainActivity extends AppCompatActivity {
 
         // tomar los dos digitos del dia del nacimiento
 
-        // generar  3 caracteres (Letras mayusculas o numeros )para la homonimia
+         // generar  3 caracteres (Letras mayusculas o numeros )para la homonimia
 
 
 
